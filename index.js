55325ef01763d920e7d4d7292120ae71fbae7c45
Doris Helena <doris90nunes@gmail.com>
2021-04-15 16:39:21 -0300

    teste.js

--- index.js
+++ index.js
@@ -16,3 +16,4 @@ app.get('/user', (req, res) => {
     }
 )
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+
---
+++ teste.js
@@ -0,0 +1,5 @@
+let n1=3;
+let n2=9;
+let soma=(n1+n2);
+
+console.log(soma);
