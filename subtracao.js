7aaf5faefd5ee778748a51a9c5e9c8f057cada6f
Doris Helena <doris90nunes@gmail.com>
2021-04-16 16:48:32 -0300

    subtracao

---
+++ subtracao.js
@@ -0,0 +1,9 @@
+//let valor1=23;
+//let valor2=12;
+//let subtracao = (valor1-valor2); //11 
+
+let valor3=12;
+let valor4=23;
+
+let subtracao = (valor3-valor4);
+console.log('o valor da subtraçao é : ' + subtracao );//-11s
